b6165b659332b18ccadbf8ca6b2fb2c86c27d72d
晨 黎 <lichen@neo.org>
2021-03-02 10:24:54 +0800

    add cors test controller

---
+++ WebApiService/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
+
+
+namespace WebApiService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        [DisableCors]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpPost("{id}")]
+        [EnableCors]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
